ba5f3a20e09fbe681e25f5afe7e8e3d4a828bbf0
Iaroslav <isindiak@gmail.com>
2023-05-06 15:01:49 -0700

    added api post comments for BIO

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -22,19 +22,70 @@ const key = "5521499d-cfcd-4d46-a655-ade2fb067982";
 const dynamicBox = document.querySelector(".comment-box__dynamic"); //new placeholder for comments
 const formBox = document.querySelector(".comment-box__form"); //placeholder for comment Form
 
+// const formBox = document.querySelector(".comment-box__title"); //last element before we start DOM manipulation
+//create form dynamically
+const form = document.createElement("form");
+form.classList.add("comment-box__form-custom");
+// create <div> for the avatar for better structure
+const avatarDynamicBox = document.createElement("div");
+avatarDynamicBox.classList.add("comment-box__avatar-box");
+//create avatar
+const formAvatar = document.createElement("img");
+formAvatar.classList.add("comment-box__avatar");
+formAvatar.src = "assets/images/Mohan-muruge.jpg";
+formAvatar.alt = "avatar";
+//create <div> for alligning input boxes
+const formDiv = document.createElement("div");
+formDiv.classList.add("comment-box__wrapper");
+//create labels
+const labelName = document.createElement("label");
+const labelComment = document.createElement("label");
+labelName.classList.add("comment-box__label");
+labelComment.classList.add("comment-box__label");
+labelName.for = "";
+labelComment.for = "";
+labelName.innerText = "name";
+labelComment.innerText = "comment";
+//create an input for Username
+const inputName = document.createElement("input");
+inputName.classList.add("comment-box__input-name");
+inputName.type = "text";
+inputName.name = "name";
+inputName.placeholder = "Enter your name";
+//create a text area for Comment
+const inputComment = document.createElement("textarea");
+inputComment.classList.add("comment-box__input-comment");
+inputComment.name = "comment";
+inputComment.placeholder = "Add a new comment";
+//create a button
+const formButton = document.createElement("button");
+formButton.classList.add("comment-box__button");
+formButton.type = "submit";
+formButton.innerText = "comment";
+
+//Append all elements to the page
+formBox.appendChild(form);
+form.appendChild(avatarDynamicBox);
+avatarDynamicBox.appendChild(formAvatar);
+form.appendChild(formDiv);
+formDiv.appendChild(labelName);
+formDiv.appendChild(inputName);
+formDiv.appendChild(labelComment);
+formDiv.appendChild(inputComment);
+formDiv.appendChild(formButton);
+
 function getComments() {
     axios
       .get(`https://project-1-api.herokuapp.com/comments?api_key=${key}`)
       .then((response) => {
-        console.log(response.data);
+        console.log(`get response: ${response.data}`);
   
         dynamicBox.innerText = "";
   
         //sort by timestamp/date the response.data
         const sortedByDate = response.data.sort((a, b) => b.timestamp - a.timestamp)
-        console.log(sortedByDate);
 
-        for (let i = 0; i < response.data.length; i++) {
+        for (let i = 0; i < sortedByDate.length; i++) {
             const commentBoxCard = document.createElement("div");
             commentBoxCard.classList.add("comment-box__card-default"); 
             // create <div> for the left part of the default card (avatar)
@@ -52,17 +103,17 @@ function getComments() {
             // create <h3> for name
             const defaultName = document.createElement("h3");
             defaultName.classList.add("comment-box__name-default");
-            defaultName.innerText = response.data[i].name;
+            defaultName.innerText = sortedByDate[i].name;
             // create <p> for date
             const defaultDate = document.createElement("p");
             defaultDate.classList.add("comment-box__date-default");
-            const datePosted = response.data[i].timestamp;
+            const datePosted = sortedByDate[i].timestamp;
             const formattedDate = new Date(datePosted).toLocaleDateString();
             defaultDate.innerText = formattedDate;
             // create <p> for comment
             const defaultComment = document.createElement("p");
             defaultComment.classList.add("comment-box__comment-default");
-            defaultComment.innerText = response.data[i].comment;
+            defaultComment.innerText = sortedByDate[i].comment;
 
             // append nodes to DOM
 
@@ -85,123 +136,30 @@ function getComments() {
 getComments();
 
 
-function postNewComment(name, comment) {
-    axios
-      .post(`https://project-1-api.herokuapp.com/comments?api_key=${key}`, {
-        name: name,
-        comment: comment,
-      })
-      .then((response) => {
-        dynamicBox.innerText = "";
-        for (let i = 0; i < response.data.length; i++) {
-        //card for dynamic comments
-    const commentBoxCardDynamic = document.createElement("div");
-    commentBoxCardDynamic.classList.add("comment-box__card-default"); 
-    // create <div> for the left part of the dynamic card (avatar)
-    const avatarBoxDynamic = document.createElement("div");
-    avatarBoxDynamic.classList.add("comment-box__avatar-box");
-    //create <div> wrapper for the right part of the dynamic card (name/date/comment)
-    const contentBoxDynamic = document.createElement("div");
-    contentBoxDynamic.classList.add("comment-box__wrapper-default");
-    // create <div> avatar
-    const avatarDynamic = document.createElement("div");
-    avatarDynamic.classList.add("comment-box__avatar-default");
-    // create <div> for name and date
-    const commentTopBoxDynamic = document.createElement("div");
-    commentTopBoxDynamic.classList.add("comment-box__top-box");
-    //post your Name
-    const dynamicName = document.createElement("h3")
-    dynamicName.classList.add("comment-box__name-default");
-    dynamicName.innerText = note.title;
-
-    // post your Date NOT WORKING yet
-    const dynamicDate = document.createElement("p");
-    dynamicDate.classList.add("comment-box__date-default");
-    // const bioDate = new Date(note.timestamp);
-    // getDate = bioDate.toLocaleString("en-US");
- 
-    //post your Comment
-    const dynamicComment = document.createElement("p");
-    dynamicComment.classList.add("comment-box__comment-default");
-    dynamicComment.innerText = note.content;
-    //append all elements to the card
-    commentBoxCardDynamic.appendChild(avatarBoxDynamic);
-    avatarBoxDynamic.appendChild(avatarDynamic);
-    commentBoxCardDynamic.appendChild(contentBoxDynamic);
-    contentBoxDynamic.appendChild(commentTopBoxDynamic);
-    commentTopBoxDynamic.appendChild(dynamicName);
-    commentTopBoxDynamic.appendChild(dynamicDate);
-    contentBoxDynamic.appendChild(dynamicComment);
-        dynamicBox.appendChild(commentBoxCardDynamic);
-    }
-        console.log(response);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-}
+// function postNewComment() {
+//         getComments();
+// }
   
 formBox.addEventListener("submit", (event) => {
-    event.preventDefault(); // MAYBE DONT NEED
-    axios
-    .get(`https://project-1-api.herokuapp.com/comments?api_key=${key}`)
-    .then((response) => {
-      console.log(response.data);
-      formBox.innerText = "";
-    for (let i = 0; i < response.data.length; i++) {
-        //create form dynamically
-        formBox.classList.add("comment-box__form");
-        // create <div> for the avatar for better structure
-        const avatarDynamicBox = document.createElement("div");
-        avatarDynamicBox.classList.add("comment-box__avatar-box");
-        //create avatar
-        const formAvatar = document.createElement("img");
-        formAvatar.classList.add("comment-box__avatar");
-        formAvatar.src = "assets/images/Mohan-muruge.jpg";
-        formAvatar.alt = "avatar";
-        //create <div> for alligning input boxes
-        const formDiv = document.createElement("div");
-        formDiv.classList.add("comment-box__wrapper");
-        //create labels
-        const labelName = document.createElement("label");
-        const labelComment = document.createElement("label");
-        labelName.classList.add("comment-box__label");
-        labelComment.classList.add("comment-box__label");
-        labelName.for = "";
-        labelComment.for = "";
-        labelName.innerText = "name";
-        labelComment.innerText = "comment";
-        //create an input for Username
-        const inputName = document.createElement("input");
-        inputName.classList.add("comment-box__input-name");
-        inputName.type = "text";
-        inputName.name = "username";
-        inputName.placeholder = "Enter your name";
-        //create a text area for Comment
-        const inputComment = document.createElement("textarea");
-        inputComment.classList.add("comment-box__input-comment");
-        inputComment.name = "comment";
-        inputComment.placeholder = "Add a new comment";
-        //create a button
-        const formButton = document.createElement("button");
-        formButton.classList.add("comment-box__button");
-        formButton.type = "submit";
-        formButton.innerText = "comment";
-
-        //Append all elements to the page
-        formBox.appendChild(avatarDynamicBox);
-        avatarDynamicBox.appendChild(formAvatar);
-        formBox.appendChild(formDiv);
-        formDiv.appendChild(labelName);
-        formDiv.appendChild(inputName);
-        formDiv.appendChild(labelComment);
-        formDiv.appendChild(inputComment);
-        formDiv.appendChild(formButton);
-    }
+    event.preventDefault();
     // posting comment to API
-    postNewComment(event.target.name.value, event.target.comment.value);
-})
+    axios
+      .post(`https://project-1-api.herokuapp.com/comments?api_key=${key}`, {
+        name: event.target.name.value,
+        comment: event.target.comment.value,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .then(() => {
+        getComments();
+        event.target.reset();
+  })
+    .catch((error) => {
+        console.error(error);
+    });
 });
+
 
 
 
